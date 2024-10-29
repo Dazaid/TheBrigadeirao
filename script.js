bcc4e769ea4962af5b83aea000f6021e51e1f7a0
Dazaid <dazaink.dzk@gmail.com>
2024-10-29 15:36:34 +0000

    mobile responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById("dropdownMenu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+  }
+  
+  function closeMenu() {
+    document.getElementById("dropdownMenu").style.display = "none";
+  }
+  
